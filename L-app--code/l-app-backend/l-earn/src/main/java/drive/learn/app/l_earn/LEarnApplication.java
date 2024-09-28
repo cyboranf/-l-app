c3083b6deeb66668dbb750db6b481f3cff711b08
cyboran <cyboranf@gmail.com>
2024-09-28 22:50:13 +0200

    backend -init

---
+++ L-app--code/l-app-backend/l-earn/src/main/java/drive/learn/app/l_earn/LEarnApplication.java
@@ -0,0 +1,13 @@
+package drive.learn.app.l_earn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LEarnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LEarnApplication.class, args);
+	}
+
+}
---
+++ L-app--code/l-app-backend/l-earn/src/test/java/drive/learn/app/l_earn/LEarnApplicationTests.java
@@ -0,0 +1,13 @@
+package drive.learn.app.l_earn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LEarnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
